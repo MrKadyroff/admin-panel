7eb5caa906d011c872ca703cc4f63d0b22323c2d
Ibraim <ibraim253@gmail.com>
2022-09-08 22:59:17 +0600

    styled solved

--- src/Layout/dashboard/index.tsx
+++ src/Layout/dashboard/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
+import { styled } from "@mui/system";
 
 const RootStyle = styled("div")(({ theme }) => ({
-  //   [theme.breakpoints.up("lg")]: {
-  display: "flex",
-  minHeight: "100%",
-  //   },
+  [theme.breakpoints.up("lg")]: {
+    display: "flex",
+    minHeight: "100%",
+  },
 }));
 
 export function DashboardLayout() {
